91b9129b11dbfc68ba4bf3cc84e6a1f9b3819af6
jooaquinn <jcenderelli@mail.austral.edu.ar>
2026-05-18 19:52:21 -0300

    Update powers.py

--- powers.py
+++ powers.py
@@ -6,8 +6,11 @@ def power(base, exp):
     exp es siempre >= 0.
 
     Ejemplo: power(2, 3) -> 8  (2*2*2)
-    """
-    return "ANSWER HERE"  # Remove this line and implement
+    "" 
+    resultado =  1
+    for i in range(exp):
+        resultado = resultado * base
+    return resultado
 
 
 def sum_of_powers(base, max_exp):
@@ -17,4 +20,7 @@ def sum_of_powers(base, max_exp):
 
     Ejemplo: sum_of_powers(2, 3) -> 15  (1+2+4+8)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    suma = 0
+    for n in range(max_exp + 1):
+        suma = suma + power(base, n)
+    return suma
